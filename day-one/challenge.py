455d20b423be5fa33e6afa4cc29f35167c87d185
Javanshir <jpashayev@aol.com>
2020-03-10 13:41:38 -0700

    Clean up day one and day two

--- day-one/challenge.py
+++ day-one/challenge.py
@@ -1,34 +1,51 @@
-
-        sum_val = sum_val + dimport math
-
-def make_list():
+def get_fuel_module_masses():
     num_list = []
-    with open ('input.txt') as f:
-        content = f.readlines()
-    for value in content:
-        num_list.append(int(value))
+    with open('input.txt') as f:
+        for line in f:
+            mass = int(line.strip())
+            num_list.append(mass)
     return num_list
 
-def calc_sum(num_list):
-    summ = 0
-    for value in num_list:
-        temp = value
-        temp = fuel_module(temp)
-        summ = summ + temp
-    print "The answer is: "
-    print summ
-    return summ
 
-def fuel_module(d):
-    sum_val = 0;
-    while d >= 9:
-        d = d//3
-        d = d - 2
-    return sum_val
+def get_module_fuel(fuel_module_mass):
+    return fuel_module_mass // 3 - 2
 
-def main():
-    number_list = make_list()
-    calc_sum(number_list)
+
+def get_filler_fuel(fuel_module_mass):
+    total_filler_fuel = 0
+    while fuel_module_mass >= 9:
+        fuel_module_mass = fuel_module_mass // 3 - 2
+        total_filler_fuel += fuel_module_mass
+    return total_filler_fuel
+
+
+def get_total_fuel(fuel_module_masses):
+    total_fuel = 0
+    for fuel_module_mass in fuel_module_masses:
+        total_fuel += get_module_fuel(fuel_module_mass)
+    return total_fuel
+
+
+def get_total_filler_fuel(fuel_module_masses):
+    total_fuel = 0
+    for fuel_module_mass in fuel_module_masses:
+        total_fuel += get_filler_fuel(fuel_module_mass)
+    return total_fuel
+
+
+def part_one():
+    fuel_module_masses = get_fuel_module_masses()
+    total_fuel = get_total_fuel(fuel_module_masses)
+    print(f'The total fuel requirements are {total_fuel}')
+
+
+def part_two():
+    fuel_filler_mass = get_fuel_module_masses()
+    total_filler_fuel = get_total_filler_fuel(fuel_filler_mass)
+    print(f'The total fuel requirements are {total_filler_fuel}')
+    # pass
+
 
 if __name__ == '__main__':
-    main()
+    part_one()
+    part_two()
